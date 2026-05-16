a5e6cc94add60a6c4800b05e79ce00a4a94be366
Andrew @ Conduit <andrew@conduit.ai>
2026-05-15 21:36:29 -0500

    chore: publish @openconduit/core to GitHub Packages

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -8,6 +8,18 @@ export type { AppService } from './services/appService';
 // Root component
 export { default as App } from './App';
 
+// Components (individually exported for use in non-desktop contexts)
+export { default as ChatArea } from './components/ChatArea';
+export { default as Sidebar } from './components/Sidebar';
+export { default as SettingsPanel } from './components/SettingsPanel';
+export { default as MessageBubble } from './components/MessageBubble';
+export { default as InputBar } from './components/InputBar';
+export { default as TopBar } from './components/TopBar';
+export { default as ToolCallCard } from './components/ToolCallCard';
+export { default as ContextWarningBanner } from './components/ContextWarningBanner';
+export { default as SystemPromptEditor } from './components/SystemPromptEditor';
+export { default as TasksPanel } from './components/TasksPanel';
+
 // Stores
 export { useConversationStore } from './stores/conversationStore';
 export { useSettingsStore } from './stores/settingsStore';
